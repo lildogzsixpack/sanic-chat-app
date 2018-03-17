1435c012c190efd3ca360371786ccef02cd9efb9
George Zed <georlema@gmail.com>
2018-03-17 14:52:41 +0200

    make constants for environment variables, make index endpoint

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
+import os
 from sanic import Sanic
 from sanic.response import json
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+SANIC_HOST = os.environ.get('SANIC_HOST', '127.0.0.1')
+SANIC_PORT = os.environ.get('SANIC_PORT', 8000)
+SANIC_DEBUG = os.environ.get('SANIC_DEBUG', False)
+
 app = Sanic()
 
-@app.route('/')
-async def test(request):
-    return json({'hello': 'world'})
+# Serving index page
+app.static('/', os.path.join(BASE_DIR, 'templates/index.html'), name='index')
 
-@app.route('/2')
-async def test2(request):
+@app.route('/chat')
+async def chat(request):
     return json({'Hello2': 'World2'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host=SANIC_HOST, port=SANIC_PORT, debug=SANIC_DEBUG)
